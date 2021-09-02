a461739f31ac9db188b636dcffc663a500f06ab4
Stepan Sendun <Stepan_Sendun@epam.com>
2021-09-02 11:55:53 +0300

    done hw 6

--- import-service/serverless.ts
+++ import-service/serverless.ts
@@ -1,33 +1,94 @@
-import type { AWS } from '@serverless/typescript';
+import type { AWS } from "@serverless/typescript";
 
-import importProductsFile from '@functions/importProductsFile';
-import importFileParser from '@functions/importFileParser';
+import importProductsFile from "@functions/importProductsFile";
+import importFileParser from "@functions/importFileParser";
+import catalogBatchProcess from "@functions/catalogBatchProcess";
+
+import {
+  PG_DATABASE,
+  PG_HOST,
+  PG_PASSWORD,
+  PG_PORT,
+  PG_USERNAME,
+} from "../_config";
 
 const serverlessConfiguration: AWS = {
-  service: 'import-service',
-  frameworkVersion: '2',
+  service: "import-service",
+  frameworkVersion: "2",
   custom: {
     webpack: {
-      webpackConfig: './webpack.config.js',
+      webpackConfig: "./webpack.config.js",
       includeModules: true,
     },
   },
-  plugins: ['serverless-webpack'],
+  plugins: ["serverless-webpack"],
   provider: {
-    name: 'aws',
-    runtime: 'nodejs14.x',
+    name: "aws",
+    runtime: "nodejs14.x",
     region: "eu-west-1",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      SQS_URL: {
+        Ref: "SQSQueue",
+      },
+      SNS_ARN: {
+        Ref: "SNSTopic",
+      },
+
+      PG_HOST,
+      PG_PORT,
+      PG_DATABASE,
+      PG_USERNAME,
+      PG_PASSWORD,
     },
-    lambdaHashingVersion: '20201221',
+    lambdaHashingVersion: "20201221",
+    iamRoleStatements: [
+      {
+        Effect: "Allow",
+        Action: "sqs:*",
+        Resource: { "Fn::GetAtt": ["SQSQueue", "Arn"] },
+      },
+      {
+        Effect: "Allow",
+        Action: "sns:*",
+        Resource: {
+          Ref: 'SNSTopic'
+        },
+      },
+    ],
   },
   // import the function via paths
-  functions: { importProductsFile, importFileParser },
+  functions: { importProductsFile, importFileParser, catalogBatchProcess },
+  resources: {
+    Resources: {
+      SQSQueue: {
+        Type: "AWS::SQS::Queue",
+        Properties: {
+          QueueName: "catalogItemsQueue",
+        },
+      },
+      SNSTopic: {
+        Type: "AWS::SNS::Topic",
+        Properties: {
+          TopicName: "createProductTopic",
+        },
+      },
+      SNSSubscription: {
+        Type: "AWS::SNS::Subscription",
+        Properties: {
+          Endpoint: "any.email@gmail.com",
+          Protocol: "email",
+          TopicArn: {
+            Ref: "SNSTopic",
+          },
+        },
+      },
+    },
+  },
 };
 
 module.exports = serverlessConfiguration;
--- import-service/src/functions/importFileParser/handler.ts
+++ import-service/src/functions/importFileParser/handler.ts
@@ -1,23 +1,43 @@
 import "source-map-support/register";
 
-import { S3 } from "aws-sdk";
+import { S3, SQS } from "aws-sdk";
 import { S3Event, S3Handler } from "aws-lambda";
 import * as csv from "csv-parser";
 
 const importFileParser: S3Handler = (event: S3Event) => {
   console.log("s3 event", event);
   const s3 = new S3({ region: "eu-west-1", signatureVersion: "v4" });
+  const sqs = new SQS();
   const recordsPromises = [];
 
   for (const record of event.Records) {
     const parsePromise = new Promise((resolve, reject) => {
-      s3.getObject({
-        Bucket: "node-aws-uploaded",
-        Key: record.s3.object.key,
-      })
+      const stream = s3
+        .getObject({
+          Bucket: "node-aws-uploaded",
+          Key: record.s3.object.key,
+        })
         .createReadStream()
-        .pipe(csv())
-        .on("data", (chunk) => console.log("csv chunk", chunk))
+        .pipe(csv());
+
+      stream
+        .on("data", (chunk) => {
+          console.log("csv chunk", chunk);
+          sqs.sendMessage(
+            {
+              QueueUrl: process.env.SQS_URL,
+              MessageBody: JSON.stringify(chunk),
+            },
+            (err) => {
+              if (err) {
+                console.error("Error sending chunk to sqs", err);
+                stream.emit("error", err);
+                return;
+              }
+              console.log("Chunk has been send to sqs", chunk);
+            }
+          );
+        })
         .on("error", (error) => {
           console.error("csv error", error);
           reject(error);
